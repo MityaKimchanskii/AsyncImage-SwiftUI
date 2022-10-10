cd28e9329a1bc99bd16c4960033aa564d6852ac9
Dmitrii Kim <mitya_kim@icloud.com>
2022-10-09 22:18:31 -0700

    Initial Commit

---
+++ AsyncImage/AsyncImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncImageApp.swift
+//  AsyncImage
+//
+//  Created by Mitya Kim on 10/9/22.
+//
+
+import SwiftUI
+
+@main
+struct AsyncImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
